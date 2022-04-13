5d0704410d87a1404d15cf80fc820d236225ed00
juanpaat <juan.alzate19@udea.edu.co>
2022-04-13 02:40:50 +0000

    3 inicial graphs

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -1,25 +1,40 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(readxl)
+library(ggplot2)
+library(ggpubr)
+library(tidyr)
+library(dplyr)
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
+      output$distPlot <- renderPlot({
+      
+      data<-read_excel("/cloud/project/daily_routine.xlsx")
+      data2 <- read_excel("/cloud/project/daily_routine.xlsx",sheet = "Sheet3")
+      data2 <- data2 %>%
+        gather(key="variable",
+               value="valor",
+               names(data2)[2:10])
+      
+       a <- ggplot(data=data, aes(x=Date, y=J_O))+
+        geom_line()+
+         ggtitle(input$variables)
+      
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+       b <- ggplot(data)+
+         geom_point(aes(x=Date, y=Go_bed))+
+         geom_point(aes(x=Date, y=Wake_up))+
+         geom_segment(aes(x=Date, xend=Date, y=Go_bed, yend=Wake_up))
+       
+       c <-ggplot(data2)+
+         geom_tile(aes(x=Date,y=variable,fill=as.factor(valor)))
+       
+       
+       
+       ggarrange(a , b , c, ncol = 1, nrow = 3)
+       
 
     })
 
--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -1,28 +1,22 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(readxl)
+library(ggplot2)
+library(ggpubr)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Dashboar - Daily Tracker"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 2,
-                        max = 50,
-                        value = 30)
+          checkboxGroupInput(
+            inputId="variables",
+            label="Choose the variables",
+            choices = c("Hola"="Hola","Chao"="Chao")
+          ),
         ),
         
         # Show a plot of the generated distribution
